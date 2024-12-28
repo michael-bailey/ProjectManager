77fbcd8e078c2ffaaa6baa4abfa29be1ae9fa42b
michael-bailey <mickyb18a@gmail.com>
2024-12-28 14:56:25 +0000

    Added function to fetch task using simpleId

--- BackEnd/Fetcher/TaskFetcher.cs
+++ BackEnd/Fetcher/TaskFetcher.cs
@@ -2,6 +2,7 @@ using EntityLib;
 using EntityLib.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Primitives;
+using Shared.Task;
 using TaskStatus = Shared.Task.TaskStatus;
 
 namespace BackEnd.Fetcher;
@@ -56,5 +57,12 @@ public class TaskFetcher (DatabaseContext ctx)
 		ctx.Tasks.Remove(entity);
 		
 		ctx.SaveChangesAsync();
+	}
+
+	public TaskData GetTask(int simpleId)
+	{
+		return (from task in ctx.Tasks
+			where task.SimpleId == simpleId
+			select task).First().ToData();
 	}
 }
