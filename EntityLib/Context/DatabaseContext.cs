9d35fe42fab3d7ac9042c5b42653873cf80e0d44
michael-bailey <mickyb18a@gmail.com>
2024-12-30 01:23:00 +0000

    Forgot these from creating the user :D

--- EntityLib/Context/DatabaseContext.cs
+++ EntityLib/Context/DatabaseContext.cs
@@ -1,8 +1,11 @@
+using EntityLib.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntityLib.Context;
 
-public class DatabaseContext(DbContextOptions options) : DbContext(options)
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) 
+	: IdentityDbContext<UserEntity, UserRole, Guid>(options)
 {
 	public DbSet<TaskEntity> Tasks { get; set; }
 }
