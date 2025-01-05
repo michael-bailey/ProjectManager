1336cb3e9b54a3b8baaf5225806b953812d435fa
michael-bailey <mickyb18a@gmail.com>
2025-01-05 17:02:32 +0000

    Added Swagger UI to backend

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -50,12 +50,16 @@ builder.Services
         opts.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     });
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
