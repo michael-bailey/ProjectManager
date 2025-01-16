2542d25167151f28f23ff12deb36662074625943
michael-bailey <mickyb18a@gmail.com>
2025-01-16 14:38:36 +0000

    Created basic asp pages project for Object storage

---
+++ ObjectStorage.WWW/Program.cs
@@ -0,0 +1,26 @@
+
+using ServiceDefaults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseExceptionHandler("/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+	.WithStaticAssets();
+
+app.Run();
